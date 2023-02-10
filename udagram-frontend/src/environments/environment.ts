0db2af733d913205de523bc296163e7d083d48ee
Amelie Janetschek <amelie_janetschek@gmx.de>
2023-02-09 21:54:29 -0500

    update externam ip

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a5a181ec3cb274d3f8cc61bf948a5d1f-676356889.us-east-1.elb.amazonaws.com'
 };
 
 /*
